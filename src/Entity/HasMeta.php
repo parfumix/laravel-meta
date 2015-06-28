d367ea6515448990cfea660b9ba9a76262124998
Radu <radu.bordei89@gmail.com>
2015-06-28 16:26:00 +0300

    implement metaable funcs.

--- src/Entity/HasMeta.php
+++ src/Entity/HasMeta.php
@@ -4,5 +4,29 @@ namespace Terranet\Metaable\Entity;
 
 trait HasMeta {
 
+    /**
+     * Get meta title .
+     *
+     */
+    public function getMetaTitle() {
 
+    }
+
+    /**
+     * Get meta description .
+     *
+     * @return mixed
+     */
+    public function getMetaDescription() {
+
+    }
+
+    /**
+     * Get meta keywords ..
+     *
+     * @return mixed
+     */
+    public function getMetaKeywords() {
+
+    }
 }
