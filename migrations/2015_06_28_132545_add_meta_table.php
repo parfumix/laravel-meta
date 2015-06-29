26d14b567abe00897ca26288db2137ee03936b41
Radu <radu.bordei89@gmail.com>
2015-06-29 09:51:01 +0300

    adding migration.

---
+++ migrations/2015_06_28_132545_add_meta_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMetaTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('meta', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('metaable_id');
+            $table->string('metaable_type');
+            $table->string('key');
+            $table->string('value');
+
+            $table->unique(['metaable_id', 'metaable_type', 'key']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('meta');
+    }
+}
