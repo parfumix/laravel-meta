47bc4a9cb32ceed1b42ed431430326111509f5b0
Radu <radu.bordei89@gmail.com>
2015-06-29 09:55:48 +0300

    imporive meta function helper.

--- helpers.php
+++ helpers.php
@@ -1,8 +1,17 @@
 <?php
 
+use Terranet\Metaable\MetaManagerContract;
+
 if(! function_exists('meta')) {
 
-    function meta() {
-        #@todo render meta ..
+    /**
+     * Return meta manager instance with attributes .
+     *
+     * @param array $attributes
+     * @return mixed
+     */
+    function meta(array $attributes = array()) {
+        return app(MetaManagerContract::class)
+            ->fromArray($attributes);
     }
 }
