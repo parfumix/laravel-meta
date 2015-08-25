a8c305c58997e78d3f20049c782f2fe69a4d94e0
Radu <radu.bordei89@gmail.com>
2015-08-25 16:29:58 +0300

    adding metaTranslation migration.

---
+++ assets/migrations/2015_08_25_132250_create_meta_translations.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMetaTranslations extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('meta_seo_translations', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('meta_id')->unsigned();
+            $table->string('language_id')->unsigned();
+            $table->string('value');
+
+            $table->foreign('meta_id')->references('id')->on('meta')->onDelete('cascade');
+            $table->foreign('language_id')->references('id')->on('languages')->onDelete('cascade');
+
+            $table->unique(['meta_id', 'language_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('meta_seo_translations');
+    }
+}
