8c1d5159402d726407dcb8fa00228eccee3818a1
Radu <radu.bordei89@gmail.com>
2015-08-25 16:45:25 +0300

    adding locale.

--- src/Eloquent/HasMeta.php
+++ src/Eloquent/HasMeta.php
@@ -2,6 +2,8 @@
 
 namespace Laravel\Meta\Eloquent;
 
+use Localization as Locale;
+
 trait HasMeta {
 
     /**
@@ -9,45 +11,54 @@ trait HasMeta {
      *
      * @return mixed
      */
-    public function meta() {
-        return $this->morphMany(MetaEloquent::class, 'metaable');
+    protected function meta() {
+        return $this->morphMany(Meta::class, 'metaable');
     }
 
     /**
      * Get meta title .
-     *
+     * @param null $locale
+     * @return string
      */
-    public function getMetaTitle() {
+    public function getMetaTitle($locale = null) {
         $meta = $this->meta()
             ->where('key', 'title')
             ->first();
 
-        return ! is_null($meta) ? $meta->value : '';
+        $locale = isset($locale) ? $locale : Locale\get_active_locale();
+
+        return ! is_null($meta) ? $meta->translate($locale)->value : '';
     }
 
     /**
      * Get meta description .
      *
+     * @param null $locale
      * @return mixed
      */
-    public function getMetaDescription() {
+    public function getMetaDescription($locale = null) {
         $meta = $this->meta()
             ->where('key', 'description')
             ->first();
 
-        return ! is_null($meta) ? $meta->value : '';
+        $locale = isset($locale) ? $locale : Locale\get_active_locale();
+
+        return ! is_null($meta) ? $meta->translate($locale)->value : '';
     }
 
     /**
      * Get meta keywords ..
      *
+     * @param null $locale
      * @return mixed
      */
-    public function getMetaKeywords() {
+    public function getMetaKeywords($locale = null) {
         $meta = $this->meta()
             ->where('key', 'keywords')
             ->first();
 
-        return ! is_null($meta) ? $meta->value : '';
+        $locale = isset($locale) ? $locale : Locale\get_active_locale();
+
+        return ! is_null($meta) ? $meta->translate($locale)->value : '';
     }
 }
