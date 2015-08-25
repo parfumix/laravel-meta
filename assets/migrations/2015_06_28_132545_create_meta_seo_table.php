7a337e68824317b71601ef84eee6b0d163e5f4fd
Radu <radu.bordei89@gmail.com>
2015-08-25 17:05:21 +0300

    improve migrations.

--- assets/migrations/2015_06_28_132545_create_meta_seo_table.php
+++ assets/migrations/2015_06_28_132545_create_meta_seo_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMetaTable extends Migration {
+class CreateMetaSeoTable extends Migration {
 
     /**
      * Run the migrations.
--- assets/migrations/2015_08_25_132250_create_meta_seo_translations.php
+++ assets/migrations/2015_08_25_132250_create_meta_seo_translations.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMetaTranslations extends Migration {
+class CreateMetaSeoTranslations extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,10 +14,10 @@ class CreateMetaTranslations extends Migration {
         Schema::create('meta_seo_translations', function(Blueprint $table) {
             $table->increments('id');
             $table->integer('meta_id')->unsigned();
-            $table->string('language_id')->unsigned();
+            $table->integer('language_id')->unsigned();
             $table->string('value');
 
-            $table->foreign('meta_id')->references('id')->on('meta')->onDelete('cascade');
+            $table->foreign('meta_id')->references('id')->on('meta_seo')->onDelete('cascade');
             $table->foreign('language_id')->references('id')->on('languages')->onDelete('cascade');
 
             $table->unique(['meta_id', 'language_id']);
