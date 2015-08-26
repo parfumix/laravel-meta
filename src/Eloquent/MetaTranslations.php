1b9d2000b01f21a2d35ed610bd59d915e99dd6ab
Radu <radu.bordei89@gmail.com>
2015-08-26 12:01:03 +0300

    adding table .

--- src/Eloquent/MetaTranslations.php
+++ src/Eloquent/MetaTranslations.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class MetaTranslations extends Model {
 
+    protected $table = 'meta_seo_translations';
+
     public $fillable = ['meta_id', 'language_id', 'value'];
 
     public $timestamps = false;
